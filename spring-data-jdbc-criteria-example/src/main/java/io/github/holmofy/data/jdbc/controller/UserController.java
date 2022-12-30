ee72a9e3c244d9b96bfb89c48eded681f9cdc7a6
holmofy <hff1996723@163.com>
2022-12-30 12:58:21 +0800

    fix maven config

--- spring-data-jdbc-criteria-example/src/main/java/io/github/holmofy/data/jdbc/controller/UserController.java
+++ spring-data-jdbc-criteria-example/src/main/java/io/github/holmofy/data/jdbc/controller/UserController.java
@@ -2,7 +2,6 @@ package io.github.holmofy.data.jdbc.controller;
 
 import io.github.holmofy.data.jdbc.dao.UserDao;
 import io.github.holmofy.data.jdbc.model.User;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor(onConstructor_ = @Autowired)
 public class UserController {
 
+    @Autowired
     UserDao userDao;
 
     @GetMapping("/search")
