528ba58af68b5fc2d9df152f48763215ef76d433
holmofy <hff1996723@163.com>
2022-12-30 11:46:12 +0800

    fix unit test

--- spring-data-jdbc-criteria-apt/src/test/resources/TestModel_.java
+++ spring-data-jdbc-criteria-apt/src/test/resources/TestModel_.java
@@ -1,20 +1,11 @@
 package io.github.holmofy.example;
 
-import static org.springframework.data.relational.core.sql.SQL.*;
-
-import org.springframework.data.relational.core.sql.Column;
-import org.springframework.data.relational.core.sql.Table;
+import java.lang.String;
 
 public final class TestModel_ {
-    public static final Table table = table("test_model");
-
-    public static final Column id = column("id", table);
-
-    public static final Column named_column = column("named_column", table);
-
-    public static final Column lower_camel = column("lower_camel", table);
-
-    public static final Column field1 = column("embedded_field1",table);
-
-    public static final Column field_2 = column("embedded_field_2",table);
+    public static final String id = "id";
+    public static final String withColumn = "named_column";
+    public static final String lowerCamel = "lower_camel";
+    public static final String field1 = "embedded_field1";
+    public static final String field2 = "embedded_field_2";
 }
